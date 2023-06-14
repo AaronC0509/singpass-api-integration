e6c07bdd0b99f43e9296075568b15f7e2602f376
AaronC0509 <jiazheng010509@gmail.com>
2023-06-14 18:02:01 +0800

    Fixed issues happening

--- Cryptographic/KeyGenerator.cs
+++ Cryptographic/KeyGenerator.cs
@@ -9,6 +9,9 @@ using Org.BouncyCastle.Security;
 using Org.BouncyCastle.Crypto.Parameters;
 using Org.BouncyCastle.OpenSsl;
 using System.IO;
+using Org.BouncyCastle.Asn1.Pkcs;
+using Org.BouncyCastle.Asn1.X509;
+using Org.BouncyCastle.Asn1.X9;
 
 public class KeyGenerator
 {
@@ -44,55 +47,30 @@ public class KeyGenerator
             return Base64UrlEncoder.Encode(hash);
         }
     }
-    public static ECDsa ConvertPemToECDsa(string pem)
+    public static ECDsaCng ConvertPemToECDsa(string pem)
     {
         TextReader textReader = new StringReader(pem);
         PemReader pemReader = new PemReader(textReader);
         AsymmetricCipherKeyPair keyPair = (AsymmetricCipherKeyPair)pemReader.ReadObject();
         ECPrivateKeyParameters privateKeyParameters = (ECPrivateKeyParameters)keyPair.Private;
 
-        var q = privateKeyParameters.Parameters.G.Multiply(privateKeyParameters.D);
-        var ps = Org.BouncyCastle.Asn1.Sec.SecNamedCurves.GetByOid(privateKeyParameters.PublicKeyParamSet);
-        var spec = new ECDomainParameters(ps.Curve, ps.G, ps.N, ps.H);
-        var publicKeyParameters = new ECPublicKeyParameters(q, spec);
+        var x9 = new X9ECParameters(
+            privateKeyParameters.Parameters.Curve,
+            new X9ECPoint(privateKeyParameters.Parameters.Curve, privateKeyParameters.Parameters.G.GetEncoded()),
+            privateKeyParameters.Parameters.N,
+            privateKeyParameters.Parameters.H,
+            privateKeyParameters.Parameters.GetSeed());
 
-        var x = publicKeyParameters.Q.AffineXCoord.GetEncoded();
-        var y = publicKeyParameters.Q.AffineYCoord.GetEncoded();
+        var privateKeyInfo = new PrivateKeyInfo(
+            new AlgorithmIdentifier(X9ObjectIdentifiers.IdECPublicKey, x9.ToAsn1Object()),
+            new Org.BouncyCastle.Asn1.Sec.ECPrivateKeyStructure((int)privateKeyParameters.Parameters.N.BitLength, privateKeyParameters.D, x9).ToAsn1Object());
 
-        var ms = new MemoryStream();
-        var bw = new BinaryWriter(ms);
-        bw.Write((byte)0x30);
-        bw.Write((byte)0x81);
-        bw.Write((byte)0x8f);
-        bw.Write((byte)0x30);
-        bw.Write((byte)0x1d);
-        bw.Write((byte)0x06);
-        bw.Write((byte)0x07);
-        bw.Write(new byte[] { 0x2a, 0x86, 0x48, 0xce, 0x3d, 0x01, 0x01 });
-        bw.Write((byte)0x02);
-        bw.Write((byte)0x12);
-        bw.Write(new byte[] { 0x00, 0x78, 0x00, 0xa6, 0x9d, 0xe6, 0x4a, 0x63, 0x98, 0x6b, 0xc6, 0x51, 0xd3, 0x2c, 0x2c, 0x81, 0x0f, 0x0d });
-        bw.Write((byte)0x04);
-        bw.Write((byte)0x6e);
-        bw.Write((byte)0x30);
-        bw.Write((byte)0x6c);
-        bw.Write((byte)0x02);
-        bw.Write((byte)0x01);
-        bw.Write((byte)0x01);
-        bw.Write((byte)0x04);
-        bw.Write((byte)0x20);
-        bw.Write(privateKeyParameters.D.ToByteArrayUnsigned());
-        bw.Write((byte)0xa1);
-        bw.Write((byte)0x44);
-        bw.Write((byte)0x03);
-        bw.Write((byte)0x42);
-        bw.Write((byte)0x00);
-        bw.Write(x);
-        bw.Write(y);
+        var serializedPrivateBytes = privateKeyInfo.GetDerEncoded();
 
-        var ecdsa = new ECDsaCng(CngKey.Import(ms.ToArray(), CngKeyBlobFormat.EccPrivateBlob));
+        ECDsaCng ecdsa = new ECDsaCng(CngKey.Import(serializedPrivateBytes, CngKeyBlobFormat.Pkcs8PrivateBlob));
         return ecdsa;
     }
+
     private static byte[] FromHex(string hex)
     {
         byte[] raw = new byte[hex.Length / 2];
